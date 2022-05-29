1ac048bbad1688bc195555c9166bc2d79ebded29
Pavel Kolomeitsev <pavelkolomeitsev1981@gmail.com>
2022-05-29 13:00:06 +0300

    add css styles

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as PIXI from "pixi.js";
 
-const app: PIXI.Application = new PIXI.Application();
-
-document.body.appendChild(app.view);
+const app: PIXI.Application = new PIXI.Application({
+    view: document.getElementById("canvas") as HTMLCanvasElement,
+    width: 900,
+    height: 600
+});
